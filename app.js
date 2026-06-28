fd54336d67c5dce3d92afe5bb6e83cd1c941dc65
Abhigyan Jha <jhaabhigyan0@gmail.com>
2026-06-28 19:30:13 +0530

    Prepare project for Vercel deployment

--- app.js
+++ app.js
@@ -17,8 +17,4 @@ app.get("/", (req, res) => {
   res.send("GitHub Profile Analyzer API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+module.exports = app;
